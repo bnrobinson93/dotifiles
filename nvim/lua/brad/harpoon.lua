cd0d223f5962c6d5fa364b3bac48932caa43c74c
Brad Robinson <31802085+bnrobinson93@users.noreply.github.com>
2024-01-26 18:53:30 -0600

    Update harpoon.lua

--- nvim/lua/brad/harpoon.lua
+++ nvim/lua/brad/harpoon.lua
@@ -1,23 +1,12 @@
-local status_ok, mark = pcall(require, 'harpoon.mark')
-if not status_ok then
-  vim.notify 'Unable to load harpoon mark'
-  return
-end
-
-local status_ok2, ui = pcall(require, 'harpoon.ui')
-if not status_ok2 then
-  vim.notify 'Unable to load harpoon ui'
-  return
-end
-
 return {
   'ThePrimeagen/harpoon',
+  dependencies = { 'nvim-lua/plenary.nvim' },
   keys = {
-    { '<leader>a', mark.add_file,                 desc = "Add file to harpoon" },
-    { '<C-e>',     ui.toggle_quick_menu,          desc = "Toggle harpoon menu" },
-    { '<leader>1', function() ui.nav_file(1) end, desc = "Go to file 1" },
-    { '<leader>2', function() ui.nav_file(2) end, desc = "Go to file 2" },
-    { '<leader>3', function() ui.nav_file(3) end, desc = "Go to file 3" },
-    { '<leader>4', function() ui.nav_file(4) end, desc = "Go to file 4" },
+    { '<leader>a', "<cmd>lua =require('harpoon.mark').add_file()<CR>", desc = 'Add file to harpoon' },
+    { '<C-e>', "<cmd>lua =require('harpoon.ui').toggle_quick_menu()<CR>", desc = 'Toggle harpoon menu' },
+    { '<leader>1', "<cmd>lua =require('harpoon.ui').nav_file(1)<CR>", desc = 'Go to file 1' },
+    { '<leader>2', "<cmd>lua =require('harpoon.ui').nav_file(2)<CR>", desc = 'Go to file 2' },
+    { '<leader>3', "<cmd>lua =require('harpoon.ui').nav_file(3)<CR>", desc = 'Go to file 3' },
+    { '<leader>4', "<cmd>lua =require('harpoon.ui').nav_file(4)<CR>", desc = 'Go to file 4' },
   },
 }
