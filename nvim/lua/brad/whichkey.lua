ea01b4905581892761a274b0ddd734d92f7f90a1
Brad Robinson <31802085+bnrobinson93@users.noreply.github.com>
2024-01-26 18:55:20 -0600

    Update whichkey.lua

--- nvim/lua/brad/whichkey.lua
+++ nvim/lua/brad/whichkey.lua
@@ -3,11 +3,13 @@ return {
   event = 'VeryLazy',
   init = function()
     vim.o.timeout = true
-    vim.o.timeoutlen = 300
+    vim.o.timeoutlen = 3000
   end,
   opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
+    plugins = {
+      presets = {
+        motion = false,
+      },
+    },
   },
 }
