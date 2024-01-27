fb7bd999050e2523b40ce805ac77e804e73b1c00
Brad Robinson <31802085+bnrobinson93@users.noreply.github.com>
2024-01-26 18:53:13 -0600

    Update autopair.lua

--- nvim/lua/brad/autopair.lua
+++ nvim/lua/brad/autopair.lua
@@ -1,9 +1,6 @@
 return {
   'windwp/nvim-autopairs',
   event = 'InsertEnter',
-  dependencies = {
-    'hrsh7th/nvim-cmp',
-  },
   opts = {
     check_ts = true,
     ts_config = {
@@ -13,17 +10,5 @@ return {
     },
     disable_filetype = { 'TelescopePrompt', 'vim' },
   },
-  config = function()
-    local status_ok, cmp_autopairs = pcall(require, 'nvim-autopairs.completion.cmp')
-    if not status_ok then
-      return
-    end
-
-    local status2_ok, cmp = pcall(require, 'cmp')
-    if not status2_ok then
-      return
-    end
-
-    cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
-  end,
 }
+
