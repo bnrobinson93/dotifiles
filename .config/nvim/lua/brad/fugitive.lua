04393c035cfb93c1e9253169cdceba8090f7d328
Brad Robinson <31802085+bnrobinson93@users.noreply.github.com>
2024-05-17 17:29:57 -0500

    Auto load if in git directory

--- .config/nvim/lua/brad/fugitive.lua
+++ .config/nvim/lua/brad/fugitive.lua
@@ -1,12 +1,13 @@
 return {
   'tpope/vim-fugitive',
+  cond = require('telescope.utils').get_os_command_output({ 'git', 'rev-parse', '--is-inside-work-tree'
   keys = {
-    { '<leader>gs', '<cmd>Git<cr>', desc = 'Git status' },
-    { '<leader>gd', '<cmd>Git diff<cr>', desc = 'Git diff' },
-    { '<leader>gf', '<cmd>Git fetch<cr>', desc = 'Git fetch' },
-    { '<leader>gp', '<cmd>Git pull<cr>', desc = 'Git pull' },
-    { '<leader>gg', '<cmd>Git push<cr>', desc = 'Git push' },
-    { '<leader>gf', '<cmd>diffget //2<cr>', desc = 'Git diffget 2' },
-    { '<leader>gj', '<cmd>diffget //3<cr>', desc = 'Git diffget 3' },
+    { '<leader>gs', '<cmd>Git<cr>', desc = '[G]it [S]tatus' },
+    { '<leader>gd', '<cmd>Gvdiffsplit!<cr>', desc = '[G]it [D]iff in vertical split' },
+    { '<leader>gF', '<cmd>Git fetch<cr>', desc = '[G]it [F]etch' },
+    { '<leader>gg', '<cmd>Git pull<cr>', desc = '[G]it [P]ull' },
+    { '<leader>gp', '<cmd>Git push<cr>', desc = '[G]it [P]ush' },
+    { '<leader>gf', '<cmd>diffget //2<cr>', desc = 'Git diffget 2 (left hand)' },
+    { '<leader>gj', '<cmd>diffget //3<cr>', desc = 'Git diffget 3 (right hand)' },
   },
 }
