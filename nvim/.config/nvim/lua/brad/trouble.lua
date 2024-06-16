6df2360ea6978aab2f9066dd72a23015fafedf54
Brad R <31802085+bnrobinson93@users.noreply.github.com>
2024-06-16 14:29:34 -0500

    Better Trouble usage

--- nvim/.config/nvim/lua/brad/trouble.lua
+++ nvim/.config/nvim/lua/brad/trouble.lua
@@ -1,32 +1,18 @@
 return {
   'folke/trouble.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
+  opts = {
+    focus = true,
+    multiline = true, -- render multi-line messages
+    auto_open = false, -- automatically open the list when you have diagnostics
+    auto_close = true, -- automatically close the list when you have no diagnostics
+    warn_no_results = false,
+  },
   keys = {
     { '<leader>tt', '<cmd>Trouble diagnostics toggle filter.buf=0<cr>', desc = 'Toggle buffer diagnostics' },
     { '<leader>td', '<cmd>Trouble diagnostics toggle<cr>', desc = 'Toggle trouble' },
     { '<leader>tq', '<cmd>Trouble qflist toggle<cr>', desc = 'Toggle trouble quickfix' },
-    {
-      ']d',
-      function()
-        local trouble = require 'trouble'
-        trouble.next { mode = 'diagnostics', skip_groups = true, jump = true }
-        trouble.close()
-      end,
-      desc = 'Next trouble',
-    },
-    {
-      '[d',
-      function()
-        local trouble = require 'trouble'
-        trouble.prev { mode = 'diagnostics', skip_groups = true, jump = true }
-        trouble.close()
-      end,
-      desc = 'Previous trouble',
-    },
-  },
-  opts = {
-    multiline = false, -- render multi-line messages
-    auto_open = false, -- automatically open the list when you have diagnostics
-    auto_close = true, -- automatically close the list when you have no diagnostics
+    { ']d', '<cmd>Trouble diagnostics next<cr><cmd>Trouble close<cr>', desc = 'Next trouble' },
+    { ']d', '<cmd>Trouble diagnostics prev<cr><cmd>Trouble close<cr>', desc = 'Prev trouble' },
   },
 }
