66c997d0e77df1c8f56c37e3f0311e0568ef400f
Brad R <31802085+bnrobinson93@users.noreply.github.com>
2024-06-19 13:12:21 -0500

    Background

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -25,6 +25,7 @@ end)
 
 return {
 	window_background_opacity = 0.97,
+	window_background_image = "./grainy (cat).jpg",
 	macos_window_background_blur = 90,
 	color_scheme = "Catppuccin Mocha",
 	font = wezterm.font("Dank Mono Nerd Font"),
@@ -33,5 +34,6 @@ return {
 	hide_tab_bar_if_only_one_tab = true,
 	keys = { { key = "F11", action = wezterm.action.ToggleFullScreen } },
 	-- window_decorations = "TITLE | RESIZE",
-	window_decorations = "RESIZE",
+	window_decorations = "TITLE | RESIZE",
+	-- integrated_title_button_style = "Gnome",
 }
