9503987a77de4dfffe20e9c5d4c38c48520f473e
Brad R <31802085+bnrobinson93@users.noreply.github.com>
2024-06-20 22:40:57 -0500

    Add alias

---
+++ nvim/.config/nvim/lua/brad/vtsls.lua
@@ -0,0 +1,17 @@
+return {
+  'yioneko/nvim-vtsls',
+  opts = {
+    settings = {
+      typescript = {
+        inlayHints = {
+          parameterNames = { enabled = 'literals' },
+          parameterTypes = { enabled = true },
+          variableTypes = { enabled = true },
+          propertyDeclarationTypes = { enabled = true },
+          functionLikeReturnTypes = { enabled = true },
+          enumMemberValues = { enabled = true },
+        },
+      },
+    },
+  },
+}
