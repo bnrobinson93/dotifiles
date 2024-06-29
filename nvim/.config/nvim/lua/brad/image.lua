e4271319eeab26d0395637dfc62bc50c0abbc47b
Brad R <31802085+bnrobinson93@users.noreply.github.com>
2024-06-28 22:26:42 -0500

    Images

---
+++ nvim/.config/nvim/lua/brad/image.lua
@@ -0,0 +1,9 @@
+return {
+  '3rd/image.nvim',
+  opts = {
+    backend = 'kitty',
+    html = { enabled = true },
+    css = { enabled = true },
+    tmux_show_only_in_active_window = true,
+  },
+}
