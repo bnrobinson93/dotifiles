4987fd8c02b2e9dc3af52682b3857cde78e9e377
Brad R <31802085+bnrobinson93@users.noreply.github.com>
2024-06-30 19:53:02 -0500

    Combined for tailwind highlighting

--- nvim/.config/nvim/lua/brad/colorizer.lua
+++ nvim/.config/nvim/lua/brad/colorizer.lua
@@ -1,9 +1,20 @@
 return {
   'NvChad/nvim-colorizer.lua',
   event = 'BufReadPre',
-  config = function()
-    require('colorizer').setup {
-      filetypes = { '*', '!vim', '!prompt', '!popup' },
-    }
-  end,
+  opts = {
+    filetypes = { '*', '!vim', '!prompt', '!popup' },
+    user_default_options = {
+      RRGGBBAA = true,
+      AARRGGBB = true,
+      css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+      css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+      mode = 'background', --- Available modes for `mode`: foreground, background, virtualtext
+      tailwind = true,
+      sass = { enable = false, parsers = { 'css' } }, -- Enable sass colors
+      virtualtext = '■',
+      -- update color values even if buffer is not focused
+      -- example use: cmp_menu, cmp_docs
+      always_update = false,
+    },
+  },
 }
--- nvim/.config/nvim/lua/brad/tailwind-colorizer.lua
+++
@@ -1,4 +0,0 @@
-return {
-  'themaxmarchuk/tailwindcss-colors.nvim',
-  ft = { 'javascript', 'typescript', 'typescriptreact', 'javascriptreact', 'css', 'markdown' },
-}
