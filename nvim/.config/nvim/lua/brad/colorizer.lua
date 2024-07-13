f441f9c8e1847a7a68898228d7c005559d64b5a0
Brad R <31802085+bnrobinson93@users.noreply.github.com>
2024-07-12 21:43:35 -0500

    cleaned up

--- nvim/.config/nvim/lua/brad/colorizer.lua
+++ nvim/.config/nvim/lua/brad/colorizer.lua
@@ -4,17 +4,12 @@ return {
   opts = {
     filetypes = { '*', '!vim', '!prompt', '!popup' },
     user_default_options = {
-      RRGGBBAA = true,
-      AARRGGBB = true,
       css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
       css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
       mode = 'background', --- Available modes for `mode`: foreground, background, virtualtext
       tailwind = true,
-      sass = { enable = false, parsers = { 'css' } }, -- Enable sass colors
+      sass = { enable = false, parsers = { 'css' } },
       virtualtext = '■',
-      -- update color values even if buffer is not focused
-      -- example use: cmp_menu, cmp_docs
-      always_update = false,
     },
   },
 }
