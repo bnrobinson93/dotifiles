93ceb56fef446f64ec7b11c7bc6216ebdfbf99e0
Brad R <31802085+bnrobinson93@users.noreply.github.com>
2024-09-13 12:54:51 -0500

    stole a bit from lazy

--- nvim/.config/nvim/lua/brad/whichkey.lua
+++ nvim/.config/nvim/lua/brad/whichkey.lua
@@ -1,11 +1,25 @@
 return {
-  'folke/which-key.nvim',
-  event = 'VeryLazy',
+  "folke/which-key.nvim",
+  event = "VeryLazy",
   opts = {
-    preset = 'modern',
+    preset = "modern",
     delay = 1000,
     plugins = {
       presets = { motion = false },
+    },
+    spec = {
+      mode = { "n", "v" },
+      { "<leader>g", group = "Git" },
+      { "<leader>p", group = "Project file/find" },
+      { "<leader>v", group = "Vim" },
+      { "<leader>t", group = "Toggle" },
+      {
+        "<leader>b",
+        group = "buffer",
+        expand = function()
+          return require("which-key.extras").expand.buf()
+        end,
+      },
     },
   },
 }
