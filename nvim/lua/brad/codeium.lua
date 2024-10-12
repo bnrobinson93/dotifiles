545f1a870d6a5bb661c9a111ffdb6071a641e131
Brad R <31802085+bnrobinson93@users.noreply.github.com>
2024-10-12 13:01:26 -0500

    Disable chat to hopefully help performance

--- nvim/lua/brad/codeium.lua
+++ nvim/lua/brad/codeium.lua
@@ -1,12 +1,9 @@
 return {
   'Exafunction/codeium.nvim',
   event = 'InsertEnter',
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-    'hrsh7th/nvim-cmp',
-  },
+  dependencies = { 'nvim-lua/plenary.nvim', 'hrsh7th/nvim-cmp' },
   config = true,
   opts = {
-    enable_chat = 'true',
+    enable_chat = 'false',
   },
 }
