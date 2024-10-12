ffbe6943f557fd8507b95e9578b7bfd3b5001387
Brad R <31802085+bnrobinson93@users.noreply.github.com>
2024-10-12 16:51:35 -0500

    tmux sub

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,4 +1,6 @@
 local wezterm = require 'wezterm'
+local sessionizer = require 'sessionizer'
+local tabbar = require 'tabbar'
 local act = wezterm.action
 
 wezterm.on('user-var-changed', function(window, pane, name, value)
@@ -34,43 +36,29 @@ return {
   font = wezterm.font_with_fallback { 'DankMono Nerd Font', 'Fira Code', 'JetBrains Mono' },
   font_size = 15,
   hide_tab_bar_if_only_one_tab = true,
+  -- window_decorations = 'INTEGRATED_BUTTONS|RESIZE',
+  window_decorations = 'TITLE|RESIZE',
 
   -- TMUX replacement
   -- Leader is the same as my old tmux prefix
-  -- leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 },
+  use_fancy_tab_bar = false,
+  leader = { key = 'b', mods = 'CTRL', timeout_milliseconds = 2000 },
   keys = {
-    -- -- splitting
-    -- {
-    -- 	mods = "LEADER",
-    -- 	key = "-",
-    -- 	action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
-    -- },
-    -- {
-    -- 	mods = "LEADER",
-    -- 	key = "|",
-    -- 	action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
-    -- },
-    -- {
-    -- 	mods = "LEADER",
-    -- 	key = "z",
-    -- 	action = wezterm.action.TogglePaneZoomState,
-    -- },
-    -- -- activate copy mode or vim mode
-    -- {
-    -- 	key = "[",
-    -- 	mods = "LEADER",
-    -- 	action = wezterm.action.ActivateCopyMode,
-    -- },
-    -- {
-    -- 	key = "p",
-    -- 	mods = "LEADER",
-    -- 	action = wezterm.action.PasteFrom("Clipboard"),
-    -- },
-    -- {
-    -- 	key = "/",
-    -- 	mods = "LEADER",
-    -- 	action = wezterm.action.Search("CurrentSelectionOrEmptyString"),
-    -- },
+    { mods = 'LEADER', key = 'c', action = act.SpawnTab 'CurrentPaneDomain' },
+    { mods = 'LEADER', key = 'x', action = act.CloseCurrentPane { confirm = true } },
+    { mods = 'LEADER', key = 'b', action = act.ActivateTabRelative(-1) },
+    { mods = 'LEADER', key = 'n', action = act.ActivateTabRelative(1) },
+    { mods = 'LEADER', key = '|', action = act.SplitHorizontal { domain = 'CurrentPaneDomain' } },
+    { mods = 'LEADER', key = '-', action = act.SplitVertical { domain = 'CurrentPaneDomain' } },
+    { mods = 'LEADER', key = 'h', action = act.ActivatePaneDirection 'Left' },
+    { mods = 'LEADER', key = 'j', action = act.ActivatePaneDirection 'Down' },
+    { mods = 'LEADER', key = 'k', action = act.ActivatePaneDirection 'Up' },
+    { mods = 'LEADER', key = 'l', action = act.ActivatePaneDirection 'Right' },
+    { mods = 'LEADER', key = 'RightArrow', action = act.AdjustPaneSize { 'Right', 5 } },
+    { mods = 'LEADER', key = 'LeftArrow', action = act.AdjustPaneSize { 'Left', 5 } },
+    { mods = 'LEADER', key = 'DownArrow', action = act.AdjustPaneSize { 'Down', 5 } },
+    { mods = 'LEADER', key = 'UpArrow', action = act.AdjustPaneSize { 'Up', 5 } },
+    { mods = 'LEADER', key = 'f', action = wezterm.action_callback(sessionizer.toggle) },
     { key = 'F11', action = wezterm.action.ToggleFullScreen },
   },
   -- search_mode = {
